3f15bf87b624928912830920e3e62901299fe95e
nghiatnguyen <51202370@hcmut.edu.vn>
2016-02-26 22:36:12 +0700

    Week 4 get all data successfully

---
+++ Exercise/Feb_26/restful-java/src/main/java/vn/khmt/restful/User.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.khmt.restful;
+
+/**
+ *
+ * @author TRONGNGHIA
+ */
+public class User {
+    
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String status;        
+    private String name;
+
+    public User(){
+        this.id = 0;
+        this.username = "";
+        this.email = "";
+        this.name = "";
+        this.status = "";
+        this.password = "";
+    }
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Exercise/Feb_26/restful-java/src/main/java/vn/khmt/restful/UserService.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.khmt.restful;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import jdk.nashorn.internal.parser.JSONParser;
+import vn.khmt.db.ConnectToSQL;
+
+/**
+ *
+ * @author HieuNguyen
+ */
+@Path("user")
+public class UserService {
+    
+    ConnectToSQL database = new ConnectToSQL("postgresql", 
+            "ec2-54-227-253-228.compute-1.amazonaws.com", 
+            "d8viikojj42e3b",
+            "uzufecmqojhnyx",
+            "WPJGueUbd3npLKslU2BEUOmMHx"
+    );
+    
+    @POST
+    @Path("/add")
+    public Response postMsg(@QueryParam("username") String username,
+                    @QueryParam("password") String password,
+                    @QueryParam("email") String email,
+                    @QueryParam("status") String status,
+                   @QueryParam("name") String name) {
+        String query = "INSERT INTO public.user(id, username, password, email, status, name) " + "SELECT MAX(t.id) + 1" + ",'" + username + "', '" + password + "','" + email + "'," + status + ",'" + name + "' FROM public.user t;";
+        database.executeSQL(query);
+        return Response.status(201).entity("OK").build();
+    }
+    
+    @GET
+    @Path("/{param}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getMsg(@PathParam("param") int id, @HeaderParam("Authorization") String auth){
+        
+        byte[] authBytes = Base64.getDecoder().decode(auth.substring(6));         
+        String authString[] = new String(authBytes).split(":");
+        
+        boolean isAdmin = database.isAdmin(authString[0], authString[1]);
+        
+        User user = new User();
+        ResultSet result =  database.getUser(id);        
+        String username = "";
+        String email = "";
+        String password = "";
+        if (result != null) {
+            try {
+                username = result.getString("username");
+                email = result.getString("email");
+                password = result.getString("password");                       
+                if (isAdmin || (username.equals(authString[0]) && password.equals(authString[1]))){                    
+                    user.setUsername(username);
+                    user.setEmail(email);
+                    return Response.status(Response.Status.OK).entity(user).build();  
+                }                
+            } catch (SQLException ex) {
+                Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            }            
+        }   
+        return Response.status(Response.Status.UNAUTHORIZED).entity("Unauthorized").build();
+    }
+    
+    @GET
+    @Path("/all")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getUserList(@HeaderParam("Authorization") String auth) {
+        
+        byte[] authBytes = Base64.getDecoder().decode(auth.substring(6));         
+        String authString[] = new String(authBytes).split(":");
+                
+        if (database.isAdmin(authString[0], authString[1])){
+            List users = new ArrayList<User>();
+            ResultSet result =  database.getAllUsers();
+            String username = "";
+            String email = "";                        
+            try {
+                while (result.next()) {                
+                    username = result.getString("username");
+                    email = result.getString("email");
+                    User user =  new User();
+                    user.setUsername(username);
+                    user.setEmail(email);
+                    users.add(user);                                                
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+            }                  
+
+            GenericEntity<List<User>> entity = new GenericEntity<List<User>>(users) {};
+
+            return Response.status(Response.Status.OK).entity(entity).build();
+        }
+        else {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Only admin can view this information").build();
+        }               
+    }
+    
+    @PUT
+    @Path("/rename")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response updateUsername(@PathParam("param") int id, JsonObject msgBody) {
+                       
+        String name = msgBody.getString("name");
+        String username = msgBody.getString("username");
+        String query = "UPDATE public.user SET name = '" + name + "' WHERE username = '" + username + "';";
+        boolean res = database.executeSQL(query);
+        if (res){
+            return Response.status(Response.Status.OK).entity("Update Successfully").build();
+        }
+        else{
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity("Update Not Successfully").build();
+        }
+    }
+    
+}
